87f3c5e2f2ad1852926f43cf088a6c45e04b1622
sam61988 <samsamoy32@gmail.com>
2025-04-21 10:59:42 +0300

    added splashscreen

--- quranapp/lib/main.dart
+++ quranapp/lib/main.dart
@@ -1,3 +1,18 @@
-void main(){
-  runApp
+import 'package:flutter/material.dart';
+import 'package:quranapp/homescreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: Homescreen.routename,
+      routes: {Homescreen.routename: (context) => Homescreen()},
+    );
+  }
 }
